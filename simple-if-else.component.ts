c6549dc2848592ad8b9aefeea524a33db66747f2
gurpreet562 <kgurpreet562@gmail.com>
2017-10-31 20:19:38 +0530

    Create simple-if-else.component.ts

---
+++ simple-if-else.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core'
+@Component({
+selector:'app-simple-if-else',
+templateUrl:'./simple-if-else.component.html';
+})
+export class SimpleIfElseComponent{
+showPanel=true;
+}
